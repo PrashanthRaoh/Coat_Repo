eaa1d965d07b326cec888916ca3ede1b943fb8ee
Prashanth Rao <raoad@timken.com>
2024-12-04 13:56:18 +0530

    New COat repo

--- src/test/java/tests/Login.java
+++ src/test/java/tests/Login.java
@@ -2,12 +2,9 @@ package tests;
 
 import java.time.Duration;
 import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import Utility.UtilityClass;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import pages.CartPage;
